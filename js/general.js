3467d9b56e9621fb378efe5ca598872f5bd8de0e
Ivan Sibinovic <44924603+IvanSibinovic@users.noreply.github.com>
2019-08-20 19:12:51 +0200

    123
    123123

--- js/general.js
+++ js/general.js
@@ -1,19 +1,23 @@
 $('li.tags').on('click', function (e) {
   window.location.href= $(this).children('a').attr("href");
   return false;
-}
-$(document).ready(function(){
-    $(".menu, .overlaid, .tags").click(function(){
-      $('ul').toggleClass('active1');
-      $('nav').toggleClass('active2');
-  //  $('.overlaid').toggleClass('overlay-appear');
-    })
-})
+});
+
+$(".menu, .overlaid, .tags").click(function(){
+	if ($(window).width() < 1050) {
+		$('ul').toggleClass('active1');
+		$('nav').toggleClass('active2');
+		$('.overlaid').toggleClass('overlay-appear');
+		}
+	else {
+		return null;
+	}
+});
 // Logo and Navbar change on scroll:  
 $(window).scroll(function () {
   if ($(document).scrollTop() > ($('nav').height())) {
     $('nav').addClass('scrolled-nav');
-  } else {
+    } else {
     $('nav').removeClass('scrolled-nav');
-  }
-});
+    }
+  });
